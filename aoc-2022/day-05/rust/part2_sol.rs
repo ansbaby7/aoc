f17a3fe968b451558a099ee7093f976da6b80c95
ansbaby7 <ansbaby7@gmail.com>
2022-12-25 17:32:27 +0530

    Add Rust soln for day 5

---
+++ aoc-2022/day-05/rust/part2_sol.rs
@@ -0,0 +1,47 @@
+use std::fs;
+
+fn main() {
+    let data = fs::read_to_string("../input.txt").expect("Couldn't read from file");
+    let (starting_stacks, mut rearrangement_steps) = data.split_once("\n\n").unwrap();
+    // println!("{} {}", starting_stacks, rearrangement_steps);
+
+    let stack_rows: Vec<&str> = starting_stacks.split("\n").collect();
+    let stack_numbers_row = stack_rows.last().unwrap();
+
+    let num_stacks = stack_numbers_row.split(" ").filter(|x| x.parse::<u32>().is_ok()).count();
+    let num_stacks = num_stacks as u32;
+
+    let mut stacks: Vec<String> = Vec::new();
+    
+    for _ in 0..num_stacks {
+        stacks.push(String::new());
+    }
+
+    for i in (0..stack_rows.len()-1).rev() {
+        for j in (1..stack_rows[i].len()).step_by(4) {
+            stacks[j/4] += &stack_rows[i][j..j+1];
+        }
+    }
+
+    stacks = stacks.iter().map(|x| x.trim_end().to_string()).collect();
+
+    // println!("{:?}", stacks);    // stacks before rearrangement
+
+    rearrangement_steps = rearrangement_steps.trim_end();
+
+    for step in rearrangement_steps.split("\n") {
+        let numbers = step.split(" ").filter(|x| x.parse::<u32>().is_ok());
+        let numbers: Vec<u32> = numbers.map(|x| x.parse::<u32>().unwrap()).collect();
+        let (num_items, from, to) = (numbers[0] as usize, numbers[1] as usize - 1, numbers[2] as usize - 1);
+
+        let l = stacks[from].len();
+        let crates = stacks[from].split_off(l-num_items);
+        stacks[to] += &crates;
+    
+    }
+
+    for stack in &stacks {
+        print!("{}", stack.chars().last().unwrap());
+    }
+
+}

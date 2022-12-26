89026b18e9400e94416bbbb8fd0cf5bbc51cc080
ansbaby7 <ansbaby7@gmail.com>
2022-12-26 23:41:32 +0530

    Add python soln for day-07

---
+++ aoc-2022/day-07/python/part1_sol.py
@@ -0,0 +1,48 @@
+f = open("../input.txt")
+data = f.read().strip()
+lines = data.split("\n")
+
+def calc_dir_size(dir, dir_map):
+    size = 0
+    for x, y in dir_map[dir]:
+        if x == "dir":
+            size += calc_dir_size(y, dir_map)
+        else:
+            size += int(x)
+    return size 
+
+dir_map = {}
+i = 0
+path = ""
+
+while i < len(lines):
+    v = lines[i].split(" ")
+    if v[1] == "cd":
+        if v[2] == "/":
+            path = "root"
+        elif v[2] == "..":
+            path = path.rsplit("/", 1)[0]
+        else:
+            path = path + "/" + v[2]
+        i += 1
+    elif v[1] == "ls":
+        if dir_map.get(path) is None:
+            dir_map[path] = []
+            i += 1
+            while i < len(lines) and lines[i].split(" ")[0] != "$":
+                x, y = lines[i].split(" ")
+                if x == "dir":
+                    y = path + "/" + y
+                dir_map[path].append((x, y))
+                i += 1
+    
+# print(dir_map)
+
+total = 0
+for dir in dir_map:
+    k = calc_dir_size(dir, dir_map)
+    if(k <= 100000):
+        total += k
+
+print(total)
+
